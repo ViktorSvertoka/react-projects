a9d4d5710aaf5845df98a53d53f0cd090d237c40
Viktor Svertoka <victor.svertoka@gmail.com>
2024-08-31 15:52:51 +0300

    Feat(Files) Added task 05

---
+++ 05-react-jsx-green-button/script.js
@@ -0,0 +1,23 @@
+const App = ({ initialButtonText, initialClassesList }) => {
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [classesList, setClassesList] = React.useState(initialClassesList);
+
+  const onButtonClick = () => {
+    setButtonText('Hello from React');
+    setClassesList('green-btn');
+  };
+
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App initialButtonText="Click me please" initialClassesList="" />);
+
+// npx live-server --open=05-react-jsx-green-button

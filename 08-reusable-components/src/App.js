b5322b73bbdd5fcb12400256ffecb041353136ac
Viktor Svertoka <victor.svertoka@gmail.com>
2024-08-31 22:38:09 +0300

    Feat(Files) Added task 08

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+import OtherComponent from './components/OtherComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <OtherComponent />
+    </div>
+  );
+}
+
+export default App;

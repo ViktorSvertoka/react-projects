5daeaa13552e080fc39d05eb6655acefd7b1730e
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-01 23:01:24 +0300

    Feat(Files) Added task 21

---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import Post from './Post';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPosts(posts))
+      .catch(error => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map(post => <Post key={post.id} {...post} />)
+      )}
+    </>
+  );
+}
+
+export default Posts;

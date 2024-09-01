f7c942da89b4622c0b00ee355b566d91637dfbf7
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-02 00:15:29 +0300

    Feat(Files) Added task 22

---
+++ 22-useEffect-async-await/src/components/Posts.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import Post from './Post';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => response.json())
+  //     .then(posts => setPosts(posts))
+  //     .catch(error => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map(post => <Post key={post.id} {...post} />)
+      )}
+    </>
+  );
+}
+
+export default Posts;

ffc69f4a10f104a7f4ce8cff206d696abf36f6da
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-06 10:40:15 +0300

    Feat(Files) Added task 26

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,14 +1,39 @@
 import { Link, useParams } from 'react-router-dom';
 import courses from '../data/courses';
+import NotFound from './NotFound';
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 function SingleCourse() {
   const params = useParams();
   const course = courses.find(course => course.slug === params.slug);
+  if (!course) {
+    return <NotFound />;
+  }
+
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!course) {
+  //     navigate('..', { relative: 'path' });
+  //   }
+  // }, [course, navigate]);
+
+  // return (
+  //   <>
+  //     <h1>{course?.title}</h1>
+  //     <h2>{course?.slug}</h2>
+  //     <h4>{course?.id}</h4>
+  //     <Link to=".." relative="path">
+  //       All courses
+  //     </Link>
+  //   </>
+  // );
 
   return (
     <>
       <h1>{course.title}</h1>
-      <h3>{course.slug}</h3>
+      <h2>{course.slug}</h2>
       <h4>{course.id}</h4>
       <Link to=".." relative="path">
         All courses

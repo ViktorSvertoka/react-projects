4c9b5086e5f8dbd1b82047206d69d5f6e2b3b7e3
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-09 10:09:17 +0300

    Feat(Files) Added task 30

---
+++ 30-react-redux/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

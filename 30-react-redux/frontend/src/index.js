82cbeb498590d05fec2eabec840fae76389a3c28
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-10 17:47:02 +0300

    Feat(Files) Added task 30

--- 30-react-redux/frontend/src/index.js
+++ 30-react-redux/frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);

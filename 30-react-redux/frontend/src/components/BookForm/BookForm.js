dbaf0c3b4a01339be08298b9604c371e1e37fc96
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-13 17:32:46 +0300

    Feat(Files) Added task 30

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import { addBook } from '../../redux/slices/booksSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
@@ -23,6 +24,17 @@ const BookForm = () => {
       dispatch(addBook(createBookWithID({ title, author })));
       setTitle('');
       setAuthor('');
+    }
+  };
+
+  const handleAddRandomBookViaAPI = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/random-book');
+      if (res.data && res.data.title && res.data.author) {
+        dispatch(addBook(createBookWithID(res.data)));
+      }
+    } catch (error) {
+      console.log('Error fetching random book', error);
     }
   };
 
@@ -51,6 +63,9 @@ const BookForm = () => {
         <button type="submit">Add Book</button>
         <button type="button" onClick={handleAddRandomBook}>
           Add Book
+        </button>
+        <button type="button" onClick={handleAddRandomBookViaAPI}>
+          Add Random via API
         </button>
       </form>
     </div>

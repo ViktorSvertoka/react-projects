7447dd14b5f333e67006d6ffbae878c119fd9765
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-13 21:41:29 +0300

    Feat(Files) Added task 30

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import { addBook } from '../../redux/slices/booksSlice';
+import { addBook, thunkFunction } from '../../redux/slices/booksSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
 import './BookForm.css';
@@ -28,14 +27,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = async () => {
-    try {
-      const res = await axios.get('http://localhost:4000/random-book');
-      if (res.data && res.data.title && res.data.author) {
-        dispatch(addBook(createBookWithID(res.data, 'API')));
-      }
-    } catch (error) {
-      console.log('Error fetching random book', error);
-    }
+    dispatch(thunkFunction);
   };
 
   return (
--- 30-react-redux/frontend/src/redux/slices/booksSlice.js
+++ 30-react-redux/frontend/src/redux/slices/booksSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import createBookWithID from '../../utils/createBookWithID';
 
 const initialState = [];
 
@@ -23,6 +25,17 @@ const booksSlice = createSlice({
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
+
+export const thunkFunction = async (dispatch, getState) => {
+  try {
+    const res = await axios.get('http://localhost:4000/random-book');
+    if (res.data && res.data.title && res.data.author) {
+      dispatch(addBook(createBookWithID(res.data, 'API')));
+    }
+  } catch (error) {
+    console.log('Error fetching random book', error);
+  }
+};
 
 export const selectBooks = state => state.books;
 

09832583ddb597b127209ca83cdba1c7eddaa73d
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-14 13:27:33 +0300

    Feat(Files) Added task 30

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook, fetchBook } from '../../redux/slices/booksSlice';
+import { setError } from '../../redux/slices/errorSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
 import './BookForm.css';
@@ -23,6 +24,8 @@ const BookForm = () => {
       dispatch(addBook(createBookWithID({ title, author }, 'manual')));
       setTitle('');
       setAuthor('');
+    } else {
+      dispatch(setError('You must fill title and author!'));
     }
   };
 
--- 30-react-redux/frontend/src/redux/slices/errorSlice.js
+++ 30-react-redux/frontend/src/redux/slices/errorSlice.js
@@ -17,6 +17,6 @@ const errorSlice = createSlice({
 
 export const { setError, clearError } = errorSlice.actions;
 
-export const selectErrorMassage = state => state.error;
+export const selectErrorMessage = state => state.error;
 
 export default errorSlice.reducer;

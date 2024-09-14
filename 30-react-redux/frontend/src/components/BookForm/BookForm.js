d79e4043c102d24f3f0ca80c8dfc36c279dc52ad
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-14 13:43:47 +0300

    Feat(Files) Added task 30

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -30,7 +30,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = async () => {
-    dispatch(fetchBook());
+    dispatch(fetchBook('http://localhost:4000/random-book'));
   };
 
   return (
